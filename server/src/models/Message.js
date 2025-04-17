8f668df21423181eca25f2a194b4dc8411c78048
Pete Royce Saldanha <159945565+peteroyce@users.noreply.github.com>
2025-04-17 09:19:06 +0530

    Add User and Message mongoose models with reaction sub-schema

---
+++ server/src/models/Message.js
@@ -0,0 +1,19 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  emoji:   String,
+  userIds: [String],
+}, { _id: false });
+
+const schema = new mongoose.Schema({
+  room:      { type: String, required: true, index: true },
+  sender:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text:      { type: String, required: true, maxlength: 2000 },
+  reactions: [reactionSchema],
+  readBy:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+}, { timestamps: true });
+
+schema.index({ room: 1, createdAt: -1 });
+
+module.exports = mongoose.model('Message', schema);
---
+++ server/src/models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const schema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email:    { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, select: false },
+  avatar:   { type: String, default: '' },
+  lastSeen: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+schema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+schema.methods.comparePassword = (candidate, hash) => bcrypt.compare(candidate, hash);
+
+module.exports = mongoose.model('User', schema);
